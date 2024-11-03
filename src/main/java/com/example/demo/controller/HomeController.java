6bdde9d90b8aafe1f5a613644d7f4d14d5ed3475
salmaehab <67350115+salmaehab@users.noreply.github.com>
2024-11-03 16:59:08 +0200

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -16,6 +16,7 @@ public class HomeController {
     public String home(Model model, @AuthenticationPrincipal OAuth2User user)
     {
         //test
+        ///lll
         model.addAttribute("userName",user.getAttribute("name"));
         String userName =  Objects.requireNonNull(model.getAttribute("userName")).toString();
         return "You are logged in as "+ userName;
